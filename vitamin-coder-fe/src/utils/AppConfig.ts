83e99951dc62f9b1c9d770861ce71fb8db5bbdf3
kienntpk01531@gmail.com <kienntpk01531>
2024-03-03 14:38:07 +0700

    Create src

---
+++ vitamin-coder-fe/src/utils/AppConfig.ts
@@ -0,0 +1,10 @@
+export const AppConfig = {
+  site_name: "VitaminCoder",
+  title: "VitaminCoder - Chào mừng bạn đến với Vitamin Coder",
+  description:
+    "Tự học lập trình , tạo web cá nhân để nâng cao kỹ năng bản thân, có thể phục vụ cho công việc tương lai sau này và chinh phục mức lương nghìn đô",
+  locale: "vi",
+  keywords:
+    "Học lập trình, Vitamin coder , ReactJs , TypeScripts, Tìm việc làm, HTML CSS",
+  url: process.env.URLS,
+};
